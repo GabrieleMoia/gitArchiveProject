527d298821b8a7f5dbf07fb7b70ac120de8c7d8a
GabrieleMoia <gabriele.moia@icloud.com>
2019-01-31 16:58:17 +0100

    import boolean di java

--- src/main/scala/classes/Commits.scala
+++ src/main/scala/classes/Commits.scala
@@ -1,3 +1,4 @@
 package classes
+import java.lang.{Boolean => JBoolean}
 
-case class Commits(sha: String, author: Author, message: String, distinct: Boolean, url: String)
+case class Commits(sha: String, author: Author, message: String, distinct: JBoolean, url: String)
