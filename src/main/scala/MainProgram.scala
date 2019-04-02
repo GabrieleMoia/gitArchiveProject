664856d20c1fde7e826d159ebcd4909814c7087e
gabrielemoia <gabriele.moia@icloud.com>
2019-04-02 16:17:13 +0200

    Aggiunta downloader e estrattore

--- src/main/scala/MainProgram.scala
+++ src/main/scala/MainProgram.scala
@@ -1,11 +1,15 @@
-import java.io.File
-import java.net.URL
+import java.io._
+import java.net.{HttpURLConnection, URL}
+import java.util.zip.GZIPInputStream
 
+import scala.io.Source
 import classes.{Commit, GitArchive}
 import operations.dataframe.{ActorOperationDF, CommitOperationDF, EventOperationDF}
-import operations.dataset.{ActorOperationDS, EventOperationDS}
+import operations.dataset.{ActorOperationDS, CommitOperationDS, EventOperationDS}
+import org.apache.commons.io.FileUtils
 import org.apache.spark.sql._
 import org.apache.spark.{SparkConf, SparkContext}
+import org.glassfish.jersey.internal.util.PropertiesHelper
 import utils.{ActorUtils, AuthorUtils, PropertiesHelperUtil, RepoUtils}
 
 import scala.sys.process._
@@ -23,13 +27,21 @@ object MainProgram {
       .getOrCreate()
     import sqlContext.implicits._
 
-    //fileDownloader("http://data.githubarchive.org/2018-03-01-0.json.gz", "2018-03-01-0.json")
+    val applicationProperties = new PropertiesHelperUtil().getApplicationProperties()
+    val date = applicationProperties.getProperty("date")
+    val downloadLocation = applicationProperties.getProperty("downloadLocation")
 
+    //file download
+    fileDownloader("http://data.githubarchive.org/" + date + ".json.gz", date + ".json")
+    extract(downloadLocation + "/" + date + ".json.gz", date + ".json")
+
+    //json parser
     val encode = Encoders.product[GitArchive]
-    val jsonDFPublic2 = sqlContext.read.option("inferSchema", 20).json("2018-03-01-0.json").withColumnRenamed("public", "publico").withColumnRenamed("type", "tipo")
-    val gitArchiveDs: Dataset[GitArchive] = jsonDFPublic2.as[GitArchive](encode)
+    val jsonDF = sqlContext.read.option("inferSchema", 20).json(date + ".json").withColumnRenamed("public", "publico").withColumnRenamed("type", "tipo")
+    val gitArchiveDs: Dataset[GitArchive] = jsonDF.as[GitArchive](encode)
     gitArchiveDs.dropDuplicates("id")
 
+    //write on CSV
     val actor = ActorUtils.getActorDataFrame(gitArchiveDs)
     ActorUtils.actorDataFrameToCSV(actor)
 
@@ -47,19 +59,34 @@ object MainProgram {
 
     val repoCount = RepoUtils.repoDataFrameCount(repo)
     println("repo count " + repoCount)
-
-    val eventOperationDF = new EventOperationDF(sc)
-    val eventOperationDS = new EventOperationDS(sqlContext)
-
-    val de = eventOperationDF.getMaxEventPerRepoHourAndActor(gitArchiveDs.toDF())
-    de.show()
-
-    val dc = eventOperationDS.getMaxEventPerActorRepoAndHour(gitArchiveDs)
-    dc.show()
-
   }
 
-  def fileDownloader(url: String, filename: String) = {
-    new URL(url) #> new File(filename) !!
+  def fileDownloader(indirizzo: String, filename: String) = {
+    val url = new URL(indirizzo)
+    val newUrl = url.openConnection().getHeaderField("Location")
+    val filename = getFilenameFromURL(newUrl)
+    val connection = new URL(newUrl).openConnection()
+    connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11")
+    connection.connect()
+    val inputStream = connection.getInputStream
+    val applicationProperties = new PropertiesHelperUtil().getApplicationProperties()
+    FileUtils.copyInputStreamToFile(inputStream, new File(applicationProperties.getProperty("downloadLocation") + "/" + filename))
+  }
+
+  def getFilenameFromURL(urlString: String): String = {
+    val splittedUrl = urlString.split("/")
+    val size = splittedUrl.length - 1
+    splittedUrl(size)
+  }
+
+  def extract(input: String, output: String) {
+    val inputStream = new GZIPInputStream(new FileInputStream(input))
+    val outputStream = new FileOutputStream(output)
+    val w = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"))
+    for (line <- Source.fromInputStream(inputStream).getLines()) {
+      w.write(line + "\n")
+    }
+    w.close()
+    outputStream.close()
   }
 }
